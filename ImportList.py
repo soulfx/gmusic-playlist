83ec28584fe5cad94690a36dc5f363f1fd9738a7
John Elkins <soulfx@yahoo.com>
2014-06-09 02:55:14 -0500

    Update ImportList.py
    - add support for searching in the user library
    - support tsv input of the form artistTalbumTsongTsong_id where T is the tab character, song_id is the optional google song id
    - add statistics support
    - move common code and variables into a common file
    - output a tsv file that has all the found songs and their music ids, tsv file can be re-imported as needed

--- ImportList.py
+++ ImportList.py
@@ -1,106 +1,197 @@
 # Author: John Elkins <john.elkins@yahoo.com>
 # License: MIT <LICENSE>
 
-import getpass
+import re
+import datetime
 import sys
 import os
 import codecs
 import math
-from gmusicapi import Mobileclient
+from common import *
 
-# the username to use
-username = 'john.elkins@gmail.com'
+# search for tracks in the personal library, tracks found there will work
+# for you, but if you share your playlist others may not be able to play
+# some tracks
+search_personal_library = True
 
+# check to make sure a filename was given
 if len(sys.argv) != 2:
-    print 'ERROR filename is required'
+    print 'ERROR input filename is required'
+    time.sleep(3)
     exit()
 
-# try to support utf-8 playlist and track names
-filepath = sys.argv[1].decode('utf-8')
-logpath = os.path.splitext(filepath)[0]+u'.log'
-with codecs.open(filepath, encoding='utf-8', mode='r') as f:
-    tracks = f.read().splitlines()
-logfile = codecs.open(logpath, encoding='utf-8', mode='w', buffering=1)
+# log in
+api = login()
 
-# log to both the console and a text file
+# setup the input and output filenames and derive the playlist name
+input_filename = sys.argv[1].decode('utf-8')
+output_filename = os.path.splitext(input_filename)[0]
+output_filename = re.compile('_\d{14}$').sub(u'',output_filename)
+playlist_name = os.path.basename(output_filename)
+
+output_filename += u'_' + unicode(datetime.datetime.now().strftime(
+    '%Y%m%d%H%M%S'))
+log_filename = output_filename + u'.log'
+tsv_filename = output_filename + u'.tsv'
+
+# open the files
+with codecs.open(input_filename, encoding='utf-8', mode='r') as f:
+    tracks = f.read().splitlines()
+logfile = codecs.open(log_filename, encoding='utf-8', mode='w', buffering=1)
+tsvfile = codecs.open(tsv_filename, encoding='utf-8', mode='w', buffering=1)
+
+# log to both the console and log file
 def log(message):
     print message.encode(sys.stdout.encoding, errors='replace')
     logfile.write(message)
     logfile.write(os.linesep)
 
-# get the password each time so that it isn't stored in plain text
-password = getpass.getpass(username + '\'s password: ')
+# log search results to the log and to the tsv
+def log_search_results(artist,album,title,song_id):
+    # log the match results    
+    log (u'   ' + artist + u' - ' + album + u' - ' + title )
 
-# log in
-api = Mobileclient()
-logged_in = api.login(username, password)
-password = None
+    # add the found song to the tsv file
+    tsvfile.write(artist + u'\t' + album + u'\t' + title + u'\t' + song_id )
+    tsvfile.write(os.linesep)
 
-if not logged_in:
-    log('ERROR unable to login')
-    exit()
+log('Loading library')
+library = api.get_all_songs()
 
-# create the playlist
-playlist_name = os.path.basename(os.path.splitext(filepath)[0])
+# begin searching for the tracks
 log('===============================================================')
-log(u'Searching for ' +unicode(len(tracks))+ u' songs from: '+playlist_name)
+log(u'Searching for songs from: '+playlist_name)
 log('===============================================================')
 
 # keep track of stats
 no_matches = 0
 low_scores = 0
+track_count = 0
 
 # gather up the song_ids and submit as a batch
 song_ids = []
 
-# loop over the tracks
+# collect some stats on the songs
+stats = create_stats()
+
+# loop over the tracks that were read from the input file
 for track in tracks:
+    
     # skip empty lines
     if not track:
         continue
 
-    # search for the track
-    top_result = api.search_all_access(track,max_results=1).get('song_hits')
+    # parse the track info if the line is in tsv format
+    track_fields = track.split(u'\t')
+    track_artist = None
+    track_album = None
+    track_title = None
+    track_id = None
 
-    # TODO if the track isn't found in all access, then search the library
-    # for the track since it may be part of my uploads
+    if len(track_fields) == 2:
+        # skip this, but treat it as a comment
+        log(track_fields[1])
+        # write it safely into the tsv file
+        tsvfile.write(u'\t')
+        tsvfile.write(re.compile('\t').sub(u' - ',track_fields[1]))
+        tsvfile.write(os.linesep)
+        continue
+    if len(track_fields) >= 3:
+        track_artist = track_fields[0]
+        track_album = track_fields[1]
+        track_title = track_fields[2]
+        if not track_artist and not track_album and not track_title:
+            # empty record
+            continue
+    if len(track_fields) >= 4:
+        track_id = track_fields[3]
+
+    # at this point we should have a valid track
+    track_count += 1
+
+    # don't search if we already have a track id
+    if track_id and use_track_ids:
+        song_ids.append(track_id)
+        log_search_results(track_artist,track_album,track_title,track_id)
+        continue
+
+    # search for the track
+    search_results = []
+
+    # search the personal library for the track
+    if track_artist and track_title and search_personal_library:
+        search_results = [item for item in library if track_artist.lower()
+            in item.get('artist').lower() and track_title.lower()
+            in item.get('title').lower()]
+        if len(search_results) != 0:
+            result_data = {}
+            result_data[u'track'] = search_results[0]
+            result_data[u'score'] = 200
+            search_results = [result_data]
+
+    # search all access for the track
+    if len(search_results) == 0:
+        query = track
+        if track_album:
+            query = track_artist + u' ' + track_title
+        search_results = api.search_all_access(query,max_results=7).get(
+            'song_hits')
 
     # check for a result
-    if len(top_result) == 0:
+    if len(search_results) == 0:
         log(u'!! '+track)
+        tsvfile.write(track)
+        tsvfile.write(os.linesep)
         no_matches += 1
         continue
-    else:
-        # display the original track for reference
-        log(u'   '+track)
 
-    top_result = top_result[0]
+    top_result = search_results[0]
+    # if we have tsv info, perform a detailed search
+    if track_artist and track_title and track_album:
+        search_results = [item for item in search_results if track_title.lower()
+	    in item['track']['title'].lower() and track_artist.lower()
+            in item['track']['artist'].lower() and track_album.lower()
+            in item['track']['album'].lower()]
+        if len(search_results) != 0:
+            top_result = search_results[0]
 
     # gather up info about result
     song = top_result.get('track')
-    song_id = song.get('storeId')
+    song_id = song.get('storeId') if song.get('storeId') else song.get('id')
     song_title = song.get('title')
     song_artist = song.get('artist')
     song_album = song.get('album')
+    update_stats(song,stats)
 
-    # check the result score
+    # check for low quality matches
     result_score = u' + '
+    is_low_result = False
     if top_result.get('score') < 120:
-        result_score = u' - '
-        low_scores += 1
-    # sometimes google picks a song that isn't what we want at all
-    elif not song_title.lower() in track.lower():
-        result_score = u' - '
-        low_scores += 1
+        is_low_result = True
+    # wrong song
+    if not song_title.lower() in track.lower():
+        is_low_result = True
+    # wrong album
+    if track_album and track_album.lower() not in song_album.lower():
+        is_low_result = True
+    # wrong artist
+    if track_artist and track_artist.lower() not in song_artist.lower():
+        is_low_result = True
         
-    log ( result_score + song_artist + u' - ' + song_title + u' - '
-        + song_album )
+    if is_low_result:
+        result_score = u' - '
+        low_scores += 1
+
+    # display the original track for reference
+    log(result_score+track)
+
+    log_search_results(song_artist,song_album,song_title,song_id)
     
     # add the song to the id list
     song_ids.append(song_id)
 
 log('===============================================================')
-log(u'Adding '+unicode(len(song_ids))+u' found songs to: '+playlist_name)
+log(u'Adding '+unicode(len(song_ids))+' found songs to: '+playlist_name)
 log('===============================================================')
 
 # add the songs to the playlist(s)
@@ -118,25 +209,30 @@ while current_playlist <= total_playlists_needed:
     current_playlist_index = ( current_playlist - 1 ) * max_playlist_size
     current_songs = song_ids[current_playlist_index :
                              current_playlist_index + max_playlist_size]
-    api.add_songs_to_playlist(playlist_id,current_songs)
 
-    log(u' + '+current_playlist_name+u' - '+unicode(len(current_songs))
-        +' songs')
+    added_songs = api.add_songs_to_playlist(playlist_id,current_songs)
+
+    log(u' + '+current_playlist_name+u' - '+unicode(len(added_songs))+
+        u'/'+unicode(len(current_songs))+' songs')
 
     # go to the next playlist section
     current_playlist += 1
 
 # log a final status
-no_match_ratio = float(no_matches) / len(tracks)
-low_score_ratio = float(low_scores) / len(tracks)
+no_match_ratio = float(no_matches) / track_count
+low_score_ratio = float(low_scores) / track_count
 found_ratio = 1 - no_match_ratio - low_score_ratio
 
 log('===============================================================')
-log('   ' + str(len(song_ids)) + '/' + str(len(tracks)) + ' tracks imported')
+log('   ' + str(len(song_ids)) + '/' + str(track_count) + ' tracks imported')
 log(' ! ' + str(no_match_ratio) + ' percent of tracks could not be matched')
 log(' - ' + str(low_score_ratio) + ' percent of tracks had low match scores')
 log(' + ' + str(found_ratio) + ' percent of tracks had high match scores')
+log('')
+stats_results = calculate_stats_results(stats,len(song_ids))
+log_stats(log,stats_results)
 
 logfile.close()
+tsvfile.close()
 api.logout()
 
