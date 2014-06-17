3d1d19cdc8a1278d04ee625e830800ab32689a25
John Elkins <soulfx@yahoo.com>
2014-06-16 20:34:23 -0500

    Create preferences.py

---
+++ preferences.py
@@ -0,0 +1,18 @@
+
+# the username to use
+username = 'john.elkins@gmail.com'
+
+# the separator to use for detailed track information
+track_info_separator = u'\\'
+
+# the order of the track details
+track_info_order = ['title','artist','album','songid']
+
+# import and export google specific track ids
+use_track_ids = True
+
+# ignore mismatched albums
+ignore_album_mismatch = True
+
+# output debug information to the log
+debug = False
