4b5d607bb7fda8a20dcff8180706cd7b05a17c26
Sam Garrett <samdgarrett@gmail.com>
2015-08-05 22:37:29 -0400

    maint - implementing double quoting based on wikipedia examples

--- common.py
+++ common.py
@@ -87,6 +87,16 @@ def create_details(details_list):
         details[track_info_order[pos]] = nfo.strip()
     return details
 
+def handle_quote_output(aString):
+  """ See: https://en.wikipedia.org/wiki/Comma-separated_values#Basic_rules_and_examples """
+  return '"%s"' % aString.replace('"', '""')
+
+def handle_quote_input(aString):
+  if len(aString) <= 1:
+    return aString
+  elif aString[0] == '"' and aString[-1] == '"':
+    return aString[1:-1].replace('""', '"')
+
 # creates details string based off the given details dictionary
 def create_details_string(details_dict, skip_id = False):
     out_string = u''
@@ -96,7 +106,7 @@ def create_details_string(details_dict, skip_id = False):
         if len(out_string) != 0:
             out_string += track_info_separator
         try:
-            out_string += '"' + unicode(details_dict[nfo]).replace('"', "'") + '"'
+            out_string += handle_quote_output(unicode(details_dict[nfo]))
         except KeyError:
             # some songs don't have info like year, genre, etc
             pass
