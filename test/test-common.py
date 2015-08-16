9d5999d604ef8002b552c9eefa4519ff2158a16b
John Elkins <soulfx@yahoo.com>
2015-08-16 17:10:31 -0500

    moving test code into common file

--- test/test-common.py
+++ test/test-common.py
@@ -25,4 +25,19 @@ class TestCommon(unittest.TestCase):
         self.assertEqual(handle_quote_input(u'""asdf""'),u'"asdf"')
         self.assertEqual(handle_quote_input(u'"asdf"'),u'asdf')
 
+    def test_quote_unquote(self):
+        """ test for verifying the quoting and unquoting that occurs in track values """
+        test_values = (("", ""),
+                       ("bog", "bog"),
+                       ("\"bog", "\"\"\"bog\""),
+                       ("\"bog\"", "\"\"\"bog\"\"\""),
+                       ("b\"o\"g", "\"b\"\"o\"\"g\""),
+                       ("\"", "\"\"\"\""))
+        for (invalue, expected) in test_values:
+            actual_out = handle_quote_output(invalue)
+            self.assertEqual(actual_out, expected) 
+
+            actual_in = handle_quote_input(actual_out)
+            self.assertEqual(actual_in, invalue) 
+
 run_test()
